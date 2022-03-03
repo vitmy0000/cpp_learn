eecfbe6a1b0ac2810ee8d3e37c41a10e61c0f2ba
Wei Zheng <weiz.vit@gmail.com>
2022-03-03 03:00:13 +0000

    update pimpl

--- pimpl/widget.cpp
+++ pimpl/widget.cpp
@@ -3,13 +3,16 @@
 class Widget::WidgetImpl {
  public:
   void Draw(const Widget& w) const {
-    if (w.Shown())  // this call to public member function requires the
-                    // back-reference
+    // back-reference
+    if (w.Shown()) {
       std::cout << "drawing a const Widget " << num_ << '\n';
+    }
   }
 
   void Draw(const Widget& w) {
-    if (w.Shown()) std::cout << "drawing a non-const Widget " << num_ << '\n';
+    if (w.Shown()) {
+      std::cout << "drawing a non-const Widget " << num_ << '\n';
+    }
   }
 
   WidgetImpl(int num) : num_(num) {}
@@ -23,6 +26,9 @@ void Widget::Draw() const {
 }
 void Widget::Draw() {
   widget_impl_->Draw(*this);
+}
+bool Widget::Shown() const {
+    return true;
 }
 Widget::Widget() = default;
 Widget::Widget(int num) : widget_impl_{std::make_unique<WidgetImpl>(num)} {}
--- pimpl/widget.h
+++ pimpl/widget.h
@@ -4,21 +4,16 @@
 
 class Widget {
  public:
-  void Draw() const;  // public API that will be forwarded to the implementation
+  void Draw() const;
   void Draw();
-  bool Shown() const {
-    return true;
-  }  // public API that implementation has to call
+  bool Shown() const;
 
-  Widget();  // even the default ctor needs to be defined in the implementation
-             // file Note: calling draw() on default constructed object is UB
+  Widget();
   explicit Widget(int);
-  ~Widget();  // defined in the implementation file, where impl is a complete
-              // type
-  Widget(Widget&&);  // defined in the implementation file
-                     // Note: calling draw() on moved-from object is UB
+  ~Widget();
+  Widget(Widget&&);
   Widget(const Widget&) = delete;
-  Widget& operator=(Widget&&);  // defined in the implementation file
+  Widget& operator=(Widget&&);
   Widget& operator=(const Widget&) = delete;
 
  private:
